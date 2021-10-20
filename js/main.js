9332f4beaddfd48b5bf89f28ab57e837a1b6b60a
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-10-20 16:34:50 +0600

    additional way with standard method

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 const cards = document.querySelectorAll(".card");
 
-cards.forEach((card) => {
+/*cards.forEach((card) => {
   card.addEventListener("click", () => {
     removeActiveClasses();
     card.classList.add("active");
   });
-});
+});*/
 
 function removeActiveClasses() {
   cards.forEach((card) => {
     card.classList.remove("active");
+  });
+}
+
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", function () {
+    removeActiveClasses();
+    cards[i].classList.add("active");
   });
 }
